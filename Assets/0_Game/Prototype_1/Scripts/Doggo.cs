a8b085174c6d7861a63d79f842505f5323a59311
Marco Marchitelli <marchitelli97@libero.it>
2020-10-03 19:43:34 +0200

    Movement and stuff.

--- Assets/0_Game/Prototype_1/Scripts/Doggo.cs
+++ Assets/0_Game/Prototype_1/Scripts/Doggo.cs
@@ -1,22 +1,168 @@
 ﻿using UnityEngine;
 using DG.Tweening;
+using NaughtyAttributes;
 
 public class Doggo : MonoBehaviour {
     [Header("References")]
     //public Transform cam;
-    public Transform booty;
+    public Transform bodyPrefab;
     [Header("Params")]
+    public LayerMask wallMask;
     public float cellSize;
     public float moveSpeed;
     public float rotationTime = .2f;
     public float inputTreshold;
 
+    [ReadOnly]
+    [SerializeField] private Direction currentDirection;
+    [ReadOnly]
+    [SerializeField] private bool moving;
+    [ReadOnly]
+    [SerializeField] private bool wallForward, wallRight, wallLeft;
+
     private Vector3 input;
-    private bool moving;
     //private Vector3 camF;
     //private Vector3 camR;
 
     private void Update () {
+        //raycast
+        Raycast();
+
+        //input
+        ReadInput();
+
+        //camF = cam.forward;
+        //camR = cam.right;
+        //camF.y = 0;
+        //camR.y = 0;
+        //camF.Normalize();
+        //camR.Normalize();
+    }
+
+    private void Move ( Direction dir ) {
+        moving = true;
+        Tween t = null;
+
+        switch ( currentDirection ) {
+            case Direction.up:
+            switch ( dir ) {
+                case Direction.up:
+                if ( wallForward )
+                    break;
+                t = transform.DOBlendableLocalMoveBy( Vector3.forward * cellSize, moveSpeed );
+                break;
+                case Direction.down:
+                //cannot go back
+                break;
+                case Direction.right:
+                if ( wallRight )
+                    break;
+                t = transform.DOBlendableLocalMoveBy( Vector3.right * cellSize, moveSpeed );
+                transform.DOLookAt( transform.position + Vector3.right, rotationTime, AxisConstraint.Y );
+                currentDirection = dir;
+                break;
+                case Direction.left:
+                if ( wallLeft )
+                    break;
+                t = transform.DOBlendableLocalMoveBy( -Vector3.right * cellSize, moveSpeed );
+                transform.DOLookAt( transform.position - Vector3.right, rotationTime, AxisConstraint.Y );
+                currentDirection = dir;
+                break;
+            }
+            break;
+            case Direction.down:
+            switch ( dir ) {
+                case Direction.up:
+                //cannot go back
+                break;
+                case Direction.down:
+                if ( wallForward )
+                    break;
+                t = transform.DOBlendableLocalMoveBy( -Vector3.forward * cellSize, moveSpeed );
+                break;
+                case Direction.right:
+                if ( wallLeft )
+                    break;
+                t = transform.DOBlendableLocalMoveBy( Vector3.right * cellSize, moveSpeed );
+                transform.DOLookAt( transform.position + Vector3.right, rotationTime, AxisConstraint.Y );
+                currentDirection = dir;
+                break;
+                case Direction.left:
+                if ( wallRight )
+                    break;
+                t = transform.DOBlendableLocalMoveBy( -Vector3.right * cellSize, moveSpeed );
+                transform.DOLookAt( transform.position - Vector3.right, rotationTime, AxisConstraint.Y );
+                currentDirection = dir;
+                break;
+            }
+            break;
+            case Direction.right:
+            switch ( dir ) {
+                case Direction.up:
+                if ( wallLeft )
+                    break;
+                t = transform.DOBlendableLocalMoveBy( Vector3.forward * cellSize, moveSpeed );
+                transform.DOLookAt( transform.position + Vector3.forward, rotationTime, AxisConstraint.Y );
+                currentDirection = dir;
+                break;
+                case Direction.down:
+                if ( wallRight )
+                    break;
+                t = transform.DOBlendableLocalMoveBy( -Vector3.forward * cellSize, moveSpeed );
+                transform.DOLookAt( transform.position - Vector3.forward, rotationTime, AxisConstraint.Y );
+                currentDirection = dir;
+                break;
+                case Direction.right:
+                if ( wallForward )
+                    break;
+                t = transform.DOBlendableLocalMoveBy( Vector3.right * cellSize, moveSpeed );
+                break;
+                case Direction.left:
+                //cannot go back
+                break;
+            }
+            break;
+            case Direction.left:
+            switch ( dir ) {
+                case Direction.up:
+                if ( wallRight )
+                    break;
+                t = transform.DOBlendableLocalMoveBy( Vector3.forward * cellSize, moveSpeed );
+                transform.DOLookAt( transform.position + Vector3.forward, rotationTime, AxisConstraint.Y );
+                currentDirection = dir;
+                break;
+                case Direction.down:
+                if ( wallLeft )
+                    break;
+                t = transform.DOBlendableLocalMoveBy( -Vector3.forward * cellSize, moveSpeed );
+                transform.DOLookAt( transform.position - Vector3.forward, rotationTime, AxisConstraint.Y );
+                currentDirection = dir;
+                break;
+                case Direction.right:
+                //cannot go back
+                break;
+                case Direction.left:
+                if ( wallForward )
+                    break;
+                t = transform.DOBlendableLocalMoveBy( -Vector3.right * cellSize, moveSpeed );
+                break;
+            }
+            break;
+        }
+
+        if ( t == null ) {
+            moving = false;
+        }
+        else {
+            Instantiate( bodyPrefab, transform.position, Quaternion.identity );
+
+            t.SetSpeedBased( true );
+            t.SetEase( Ease.OutBack );
+            t.onComplete += MoveEndHandler;
+        }
+    }
+
+    private void ReadInput () {
         if ( moving )
             return;
 
@@ -35,46 +181,25 @@ public class Doggo : MonoBehaviour {
             else
                 Move( Direction.down );
         }
-
-        //camF = cam.forward;
-        //camR = cam.right;
-        //camF.y = 0;
-        //camR.y = 0;
-        //camF.Normalize();
-        //camR.Normalize();
     }
 
-    private void Move ( Direction dir ) {
-        moving = true;
-        Tween t = null;
+    private void Raycast () {
+        //forward
+        Ray r = new Ray(transform.position, transform.forward);
+        wallForward = Physics.Raycast( r, cellSize, wallMask );
 
-        switch ( dir ) {
-            case Direction.up:
-            t = transform.DOBlendableLocalMoveBy( Vector3.forward * cellSize, moveSpeed );
-            transform.DOLookAt( transform.position + Vector3.forward, rotationTime, AxisConstraint.Y );
-            break;                                                                               
-            case Direction.down:                                                                 
-            t = transform.DOBlendableLocalMoveBy( -Vector3.forward * cellSize, moveSpeed );        
-            transform.DOLookAt( transform.position - Vector3.forward, rotationTime, AxisConstraint.Y );
-            break;
-            case Direction.right:
-            t = transform.DOBlendableLocalMoveBy( Vector3.right * cellSize, moveSpeed );
-            transform.DOLookAt( transform.position + Vector3.right, rotationTime, AxisConstraint.Y );
-            break;
-            case Direction.left:
-            t = transform.DOBlendableLocalMoveBy( -Vector3.right * cellSize, moveSpeed );
-            transform.DOLookAt( transform.position - Vector3.right, rotationTime, AxisConstraint.Y );
-            break;
-        }
+        //right
+        r = new Ray( transform.position, transform.right );
+        wallRight = Physics.Raycast( r, cellSize, wallMask );
 
-        t.SetSpeedBased( true );
-        t.SetEase( Ease.OutBack );
-        t.onComplete += MoveEndHandler;
+        //left
+        r = new Ray( transform.position, -transform.right );
+        wallLeft = Physics.Raycast( r, cellSize, wallMask );
     }
 
     private void MoveEndHandler () => moving = false;
 
-    enum Direction { up, down, right, left }
+    public enum Direction { up, down, right, left }
 
     //private void LateUpdate () {
     //    //Vector3 velocity = ( camF * input.y + camR * input.x ) * speed * Time.deltaTime;
---
+++ Assets/Deirin/Utilities/Generic Monos/KeyCheck.cs
@@ -0,0 +1,18 @@
+﻿namespace Deirin.Utilities {
+    using UltEvents;
+    using UnityEngine;
+
+    public class KeyCheck : MonoBehaviour {
+        public KeyCode key;
+        public UltEvent OnDown;
+        public UltEvent OnUp;
+
+        private void Update () {
+            if ( Input.GetKeyDown( key ) )
+                OnDown.Invoke();
+
+            if ( Input.GetKeyUp( key ) )
+                OnUp.Invoke();
+        }
+    }
+}
